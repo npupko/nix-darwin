0184b081c3a78a6010ae62c765f2fae05f0d10a9
Nick Pupko <work.pupko@gmail.com>
2026-02-26 13:09:23 +0300

    Revert jj starship prompt integration

--- home.nix
+++ home.nix
@@ -251,63 +251,18 @@ username,
     enableZshIntegration = true;
     settings = {
       add_newline = false;
-      format = "$directory\${custom.jj}\${custom.git_branch}\${custom.git_commit}\${custom.git_status}\${custom.git_metrics}$cmd_duration$line_break$character";
+      format = "$directory$git_branch$git_state$git_status$cmd_duration$line_break$character";
       directory = {
         truncation_length = 3;
         truncate_to_repo = true;
       };
-      custom.jj = {
-        description = "The current jj status";
-        when = "jj --ignore-working-copy root";
-        symbol = "🥋 ";
-        command = ''
-          jj log --revisions @ --no-graph --ignore-working-copy --color always --limit 1 --template '
-            separate(" ",
-              change_id.shortest(4),
-              bookmarks,
-              "|",
-              concat(
-                if(conflict, "💥"),
-                if(divergent, "🚧"),
-                if(hidden, "👻"),
-                if(immutable, "🔒"),
-              ),
-              raw_escape_sequence("\x1b[1;32m") ++ if(empty, "(empty)"),
-              raw_escape_sequence("\x1b[1;32m") ++ coalesce(
-                truncate_end(29, description.first_line(), "…"),
-                "(no description set)",
-              ) ++ raw_escape_sequence("\x1b[0m"),
-            )
-          '
-        '';
+      git_branch = {
+        format = "[$branch]($style) ";
+        style = "bold purple";
       };
-      git_status.disabled = true;
-      git_commit.disabled = true;
-      git_metrics.disabled = true;
-      git_branch.disabled = true;
-      custom.git_status = {
-        when = "! jj --ignore-working-copy root";
-        command = "starship module git_status";
-        style = "";
-        description = "Only show git_status if we're not in a jj repo";
-      };
-      custom.git_commit = {
-        when = "! jj --ignore-working-copy root";
-        command = "starship module git_commit";
-        style = "";
-        description = "Only show git_commit if we're not in a jj repo";
-      };
-      custom.git_metrics = {
-        when = "! jj --ignore-working-copy root";
-        command = "starship module git_metrics";
-        style = "";
-        description = "Only show git_metrics if we're not in a jj repo";
-      };
-      custom.git_branch = {
-        when = "! jj --ignore-working-copy root";
-        command = "starship module git_branch";
-        style = "";
-        description = "Only show git_branch if we're not in a jj repo";
+      git_status = {
+        format = "[$all_status$ahead_behind]($style) ";
+        style = "bold red";
       };
       character = {
         success_symbol = "[❯](bold green)";
